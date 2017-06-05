66e69e0ee498d5518c6faf31d18244871960259d
seanrkerr <sean@seankerr.com>
2017-06-05 21:46:10 +1000

    add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,42 @@
 var webpack = require('webpack')
 var path = require('path')
+const WebpackNotifierPlugin = require('webpack-notifier')
 
 var buildFolder = path.resolve(__dirname + '/build');
 var appFolder = path.resolve(__dirname + '/app');
 
-/*
-
-
-var config = {
-  entry: appFolder + '/index.jsx'
-, output: {
-    path: buildFolder
-  , filename: 'bundle.js'
-  , publicPath: '/'
-  }
-, resolve: {
-    extensions: ['.js', '.jsx']
-  }
-, devtool: 'source-map'
-, devServer: {
-    inline: true
-  , contentBase: buildFolder
-  , port: 3000
-  }
-, module: {
-    loaders: [
-      {
-        test: /\.jsx?/
-      , include: appFolder
-      , loader: 'babel-loader'
-      , query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  }
-}
-
-module.exports = config
-*/
 module.exports = {
-    entry: appFolder + '/index.tsx'
-    , output: {
-        path: buildFolder
-        , filename: 'bundle.js'
-        , publicPath: '/'
+    devtool: 'source-map',
+    entry: [
+        appFolder+'/index.tsx'
+    ],
+    output: {
+        filename: 'app.js',
+        publicPath: 'build',
+        path: path.resolve('build')
     },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
     devServer: {
-        inline: true
-        , contentBase: buildFolder
-        , port: 3000
+        port: 3000,
+        historyApiFallback: true,
+        inline: true,
+        stats: {
+            modules: false,
+            chunks: false,
+            children: false,
+            chunkModules: false,
+            hash: false,
+        },
     },
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        modules: ['src', 'node_modules'],
     },
-
     module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        loaders: [
+            { test: /\.tsx?$/, loaders: ['babel-loader', 'ts-loader'], include: path.resolve('app') }
         ]
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
-};
+    plugins: [
+        new WebpackNotifierPlugin(),
+    ]
+}
