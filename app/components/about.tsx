cf6d61704560e4ca6c31edd27c322f8685bb862b
seanrkerr <sean@seankerr.com>
2017-06-13 21:58:50 +1000

    add some components for context

---
+++ app/components/about.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+export class About extends React.Component<{}, {}> {
+   public render() {
+        return <h1>Hello from about</h1>
+    }
+}
---
+++ app/components/home.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+export class Home extends React.Component<{}, {}> {
+   public render() {
+        return <h1>Hello from home</h1>
+    }
+}
