7646b456637c21386e2a9cbe53fe40d8e28c1e9a
seanrkerr <sean@seankerr.com>
2017-06-13 22:26:16 +1000

    chnages to the about component

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Props } from '../interfaces/Props'
+import { State } from '../interfaces/State'
 
-export class About extends React.Component<{}, {}> {
+export class About extends React.Component<Props, State> {
+     public state: State
+    public props: Props
+
+    constructor(props: Props) {
+        super(props)
+    }
    public render() {
         return <h1>Hello from about</h1>
     }
