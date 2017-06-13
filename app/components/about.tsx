97dea6bb8e88b58726c2c034c50a85de3647e9b4
seanrkerr <sean@seankerr.com>
2017-06-13 22:30:28 +1000

    linting errors

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,13 +4,13 @@ import { Props } from '../interfaces/Props'
 import { State } from '../interfaces/State'
 
 export class About extends React.Component<Props, State> {
-     public state: State
+    public state: State
     public props: Props
 
     constructor(props: Props) {
         super(props)
     }
-   public render() {
+    public render() {
         return <h1>Hello from about</h1>
     }
 }
