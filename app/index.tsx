9cb1cb35d7045377ef600b8135cffa4441a8c3d3
seanrkerr <sean@seankerr.com>
2017-06-13 21:56:13 +1000

    add default route

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { About } from './components/about'
+import { Home } from './components/home'
+import { Props } from './interfaces/Props'
+import { State } from './interfaces/State'
 
-import { Hello } from './components/Hello'
+export class App extends React.Component<Props, State> {
+    public state: State
+    public props: Props
+
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            view: <Home />,
+        }
+    }
+
+    public render() {
+            return (
+                <div>
+                    {this.state.view}
+                </div>
+            )
+    }
+}
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <App />,
     document.getElementById('app'),
 )
